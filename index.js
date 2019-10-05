b2cf43cd1eaac5eae2af71363d51a555ebea278f
Nikola Stojkovski <nikola-stojkovski@outlook.com>
2019-10-05 17:26:17 +0200

    Added import-export, request from axios and map function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var funcForGenearateString = require("randomstring");
+var axios = require('axios');
+var print = require("./second");
+
+print.main();
 
 var name = funcForGenearateString.generate({
   length: 12,
@@ -11,21 +15,72 @@ function wrapper (a,b) {
   var c = a + b;
   return new Promise((resolve, reject) => {
     if(c>10) {
-      resolve("Uspeshen promis zbirot e nad 10. zbir = " + c);
+      resolve(c);
     }
     reject("Error zbirot vi e = " + c + ", a treba da e nad 10");
   });  
 }
 
+function wrapper2 (a) {
+  return new Promise((resolve, reject) => {
+    if(a%2==0) {
+      resolve("Zbirot e paren");
+    }
+    reject("Zbirot NE e paren");
+  });  
+}
+
 async function sum(){
   try {
-    var sum = await wrapper(0,5);
-    //var result = await wraper2(sum);n
-    console.log(sum);
+    var sum = await wrapper(11,5);
+    var result = await wrapper2(sum);
+    console.log(result);
   
   } catch (error) {
     console.log(error);
   }
 }
 
-sum();
+//sum();
+
+async function getQuestions() {
+  try {
+    const response = await axios.get("https://opentdb.com/api.php?amount=10");
+    console.log(response.data);
+
+    var questionsList = response.data.results.map((obj) => {
+      return { 
+        question: obj.question,
+         category: obj.category
+        };
+    });
+
+    console.log(questionsList);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//getQuestions();
+
+async function postMail() {
+  try {
+    const response = await axios.post("http://urlexample.com",{
+      ime: "nikola",
+      prezime: "Stojkovski",
+      oglasId: 1231234
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function Execute() {
+  while(true) {
+    await postMail();
+
+  }
+}
+
+//Execute();
