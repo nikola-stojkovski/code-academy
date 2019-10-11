66b86c47dfa68e35c8c973bf4ad75a8c9a3197ab
Nikola Stojkovski <nikola-stojkovski@outlook.com>
2019-10-11 10:52:38 +0200

    Routes for user crud operations

--- web-api/app.js
+++ web-api/app.js
@@ -4,21 +4,34 @@ var bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.get('/users', (req, res) => {
+    res.status(200).send("Get All users");
+});
 
-app.get('/nikola/:number', (req, res) => {
-    var id = req.params.number;
-    console.log(id);
-    res.send(id);
+app.get('/users/:id', (req, res) => {
+    res.status(200).send("Get user with id = " + req.params.id);
 });
 
 app.post('/users', (req, res) => {
-    res.send(req.body);
+    res.send("Create user");
 });
 
-var port = process.env.PORT || 8000;
+app.put('/users/:id', (req, res) => {
+    res.send("Full update for user with id = " + req.params.id);
+});
+
+app.patch('/users/:id', (req, res) => {
+    res.send("Partial update for user with id = " + req.params.id);
+});
+
+app.delete('/users/:id', (req, res) => {
+    res.send("Delete user with id = " + req.params.id);
+});
+
+var port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`API is listenig on port ${port}!`);
 });
