dfc38e4cb6bd429d71d7c1784aaea17a23dd1f6f
Nikola Stojkovski <nikola-stojkovski@outlook.com>
2019-10-26 11:13:42 +0200

    Connected to mysql db

--- web-api/users/actions.js
+++ web-api/users/actions.js
@@ -1,28 +1,58 @@
 const fs = require('fs');
 const path = require('path');
 const { emailValidator } = require('../helper');
+const con = require('../database');
 
-getAllUsers = (req, res) => {
-    let rawdata = fs.readFileSync(path.join(__dirname, 'users.json'));
-    let users = JSON.parse(rawdata);
-    res.status(200).send(users);  
+getAllUsersQuery = () => {
+    const query = 'SELECT * FROM user';
+    return new Promise((resolve, reject) => {
+        con.query(query, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results)
+            }
+          });
+    });
 };
 
-getSpecificUser = (req, res, next) => {
-    let rawdata = fs.readFileSync(path.join(__dirname, 'users.json'));
-    let users = JSON.parse(rawdata);
-
-    if (req.params.id == 0) {
-        var error = new Error("Id can not be 0!");
-        error.status = 401;
-        next(error);
+getAllUsers = async(req, res) => {
+    try {
+        const users = await getAllUsersQuery();
+        res.status(200).send(users);  
+    } catch (error) {
+        res.status(500).send(error.message);
     }
+};
 
-    let currentUser = users.filter((x) => {
-        return x.id == req.params.id;
+getSpecificUserQuery = (userId) => {
+    const query = 'SELECT * FROM user WHERE id = ?';
+    return new Promise((resolve, reject) => {
+        con.query(query, [userId], (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results)
+            }
+          });
     });
+};
 
-    res.status(200).send(currentUser[0]);
+getSpecificUser = async(req, res, next) => {
+    const userId = req.params.id;
+
+    if (userId <= 0) {
+        var error = new Error("Id can not be less than 1!");
+        error.status = 401;
+        return next(error);
+    }
+    
+    try {
+        const user = await getSpecificUserQuery(userId);
+        res.status(200).send(user[0]);  
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
 
 createUser = (req, res, next) => {
